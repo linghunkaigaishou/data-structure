9cc39cb924d7530ccbb51d73137e585a6dd01c20
linghunkaigaishou <927793613@qq.com>
2018-11-27 17:10:34 +0800

    程序计时

--- chapter2-1.cpp
+++ chapter2-1.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <time.h>
 #define  Maxsize 100
+using namespace std;
+//using std::cout;
+//using std::endl;
 
 typedef struct 
 	{	
@@ -26,8 +31,12 @@ int del_min(sqltable &L)
 
 	}
 
-int main(void)
+int main()
 	{
+	  clock_t start,finish;
+	  double totaltime;
+	  start =clock();
+
 	  sqltable L;
 	  printf("请输入顺序表长度:");
 	  scanf("%d",&L.length);
@@ -39,7 +48,11 @@ int main(void)
 	  del_min(L);
 	  for(int i=0;i<L.length;i++)
 		printf("顺序表第%d个元素:%d\n",i+1,L.data[i]);
-			
+	  
+	  finish=clock();
+	  totaltime=(double)(finish-start)/CLOCKS_PER_SEC;
+	  cout<<"\n此程序的运行时间为"<<totaltime<<"秒!"<<endl;
+	  return 0;
 
 
 	}
